572f3ebd54441aa28bc644e5fcc10e61bc7edf25
dansigo <daniel.sigo@gmail.com>
2026-03-28 16:42:12 -0300

    feat: implemented protected routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,13 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Map from "../pages/Map";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/map" element={<Map />} />
+
+      <Route
+        path="/map"
+        element={
+          <ProtectedRoute>
+            <Map />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
